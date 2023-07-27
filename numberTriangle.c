4e9b33fc3cbec01418d079f2fc321328f8f3c7a8
Nandini-2525 <101392626+Nandini-2525@users.noreply.github.com>
2023-07-27 16:46:29 +0530

    Create numberTriangle.c

---
+++ numberTriangle.c
@@ -0,0 +1,16 @@
+// The number triangle pattern
+
+#include<stdio.h>
+int main(){
+	int n;
+	printf("Enter a number:");
+	scanf("%d",&n);
+	int i,j;
+	for(i=1;i<=n;i++){
+		for(j=1;j<=i;j++){
+			printf("%d ",j);
+		}
+		printf("\n");
+	}
+	return 0;
+}
